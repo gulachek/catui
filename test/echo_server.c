e514109d8b326ef790efcd65de45dbaffe035b4f
Nicholas Gulachek <gulac004@umn.edu>
2023-10-27 19:18:42 -0500

    make echo_server multiplex clients

--- test/echo_server.c
+++ test/echo_server.c
@@ -1,7 +1,16 @@
 #include "catui_server.h"
 #include <msgstream.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/param.h>
+#include <sys/select.h>
 #include <unistd.h>
+
+#define NCLIENTS 128
+struct client_entry {
+  int is_active;
+  int fd;
+};
 
 int main(int argc, char **argv) {
   fprintf(stderr, "Starting echo_server...\n");
@@ -14,30 +23,74 @@ int main(int argc, char **argv) {
 
   fprintf(stderr, "Load balancer at descriptor %d\n", lb);
 
+  struct client_entry clients[NCLIENTS] = {};
+
   while (1) {
-    int con = catui_server_accept(lb, stderr);
-    if (con == -1) {
-      return -1;
+    fd_set readset;
+    FD_ZERO(&readset);
+
+    int max_fd = lb;
+    FD_SET(lb, &readset);
+
+    for (int i = 0; i < NCLIENTS; ++i) {
+      struct client_entry *client = &clients[i];
+      if (!client->is_active)
+        continue;
+
+      FD_SET(client->fd, &readset);
+      max_fd = MAX(max_fd, client->fd);
     }
 
-    fprintf(stderr, "Accepted connection at descriptor %d\n", con);
-
-    if (catui_server_ack(con, stderr) == -1) {
-      return -1;
-    }
-
-    fprintf(stderr, "Sent ack\n");
-
-    char buf[1024];
-    msgstream_size nread = 0;
-    while ((nread = msgstream_recv(con, buf, sizeof(buf), stderr)) >= 0) {
-      msgstream_send(con, buf, sizeof(buf), nread, stderr);
-    }
-
-    if (nread == MSGSTREAM_EOF)
-      continue;
-    else
+    if (select(max_fd + 1, &readset, NULL, NULL, NULL) == -1) {
+      perror("select");
       return 1;
+    }
+
+    if (FD_ISSET(lb, &readset)) {
+      int con = catui_server_accept(lb, stderr);
+      if (con == -1) {
+        return -1;
+      }
+
+      struct client_entry *connected_client = NULL;
+
+      for (int i = 0; i < NCLIENTS; ++i) {
+        struct client_entry *client = &clients[i];
+        if (!client->is_active) {
+          connected_client = client;
+          break;
+        }
+      }
+
+      if (connected_client) {
+        if (catui_server_ack(con, stderr) != -1) {
+          connected_client->is_active = 1;
+          connected_client->fd = con;
+        }
+      } else {
+        catui_server_nack(con, "Max connections", stderr);
+      }
+    }
+
+    for (int i = 0; i < NCLIENTS; ++i) {
+      struct client_entry *client = &clients[i];
+      if (!(client->is_active && FD_ISSET(client->fd, &readset)))
+        continue;
+
+      char buf[1024];
+      int con = client->fd;
+
+      msgstream_size nread = msgstream_recv(con, buf, sizeof(buf), stderr);
+
+      if (nread >= 0) {
+        nread = msgstream_send(con, buf, sizeof(buf), nread, stderr);
+      }
+
+      if (nread < 0) {
+        close(con);
+        memset(client, 0, sizeof(struct client_entry));
+      }
+    }
   }
 
   return 0;
